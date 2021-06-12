0635f63654264a5aff987897fbeeefead4206ef2
Eden-Seo <85007046+Eden-Seo@users.noreply.github.com>
2021-06-12 10:45:30 +0900

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    public float speed = 8f;
+    public float rotSpeed = 120.0f;
+
+    private Transform tr;
+
+    public int hp = 100;
+    public HPBar hpbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+        Vector3 newVelocity;
+        newVelocity = new Vector3(xSpeed, -8f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
+
+        
+
+    }
+
+    public void GetDamage(int damage)
+    {
+        hp -= damage;
+        hpbar.SetHP(hp);
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void GetHeal(int heal)
+    {
+        hp += heal;
+        if(hp > 100)
+        {
+            hp = 100;
+        }
+        hpbar.SetHP(hp);
+    }
+
+     void Die()
+    {
+        gameObject.SetActive(false);
+
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.EndGame();
+    }
+}
